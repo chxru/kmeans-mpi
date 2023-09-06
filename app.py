233181d35b78bf187cb9bdab8e07caa8a3834fc6
SCARRAIDER <rsahanediriweera@gmail.com>
2023-09-06 18:37:44 +0530

    updated as dicussed in the PR

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import csv
 import os
 import pandas as pd
 
+# parrelled part
+
 np.random.seed(1234)
 
 K = 3
 M = 2
 max_iter = 10
-
-
 
 # Initialize MPI
 comm = MPI.COMM_WORLD
@@ -25,39 +25,16 @@ import os
 import csv
 import numpy as np
 
-def read_csv_files_in_directory(directory):
-    data_list = []
-
-    for filename in os.listdir(directory):
-        if filename.endswith(".csv"):
-            file_path = os.path.join(directory, filename)
-
-            with open(file_path, 'r', newline='') as csv_file:
-                csv_reader = csv.reader(csv_file)
-                data = [row for row in csv_reader]
-
-            data_list.extend(data)
-
-    numpy_array = np.array(data_list)
-
-    return numpy_array.astype(np.float64)
-
-X = read_csv_files_in_directory(csv_directory)
-
-
-
-
 csv_files = [os.path.join(csv_directory, file) for file in os.listdir(csv_directory) if file.endswith(".csv")]
 
 total_row_count = 0
 for file_path in csv_files:
-    with open(file_path, 'r') as file:
-        total_row_count += sum(1 for line in file)
+    input_file = open(file_path,'r+')
+    reader_file = csv.reader(input_file)
+    total_row_count+=len(list(reader_file))
 
 rows_per_process = total_row_count // size
-
 start_row = rank * rows_per_process
-
 end_row = start_row + rows_per_process
 
 for file_path in csv_files:
@@ -73,8 +50,8 @@ size = comm.Get_size()
 # split data into chunks
 N_per_process = N // size
 
-start_row = rank*N_per_process
-end_row = N_per_process*(rank+1)-1
+# start_row = rank*N_per_process
+# end_row = N_per_process*(rank+1)-1
 
 data = df.to_numpy()
 parallel_kmeans = ParallelKMeans(data=data, K=K, D=M)
@@ -82,6 +59,19 @@ parallel_kmeans.fit(max_iter)
 
 
 if rank == 0:
+    def read_csv_files_in_directory(directory):
+        data_list = []
+        for filename in os.listdir(directory):
+            if filename.endswith(".csv"):
+                file_path = os.path.join(directory, filename)
+                with open(file_path, 'r', newline='') as csv_file:
+                    csv_reader = csv.reader(csv_file)
+                    data = [row for row in csv_reader]
+                data_list.extend(data)
+        numpy_array = np.array(data_list)
+        return numpy_array.astype(np.float64)
+
+    X = read_csv_files_in_directory(csv_directory)
     # calculating kmeans sequentially
     sequential_kmeans = SequentialKMeans(K=K, D=M, data=X)
     sequential_kmeans.initial_centroids = parallel_kmeans.initial_centroids
