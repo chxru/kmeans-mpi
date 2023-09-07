5a726ee3f95c39e83797dff8adf766af89b5ad50
Charuka Samarakoon <charuka@protonmail.com>
2023-09-07 11:17:23 +0530

    fix data loader 2

--- data/csv_utils.py
+++ data/csv_utils.py
@@ -27,9 +27,8 @@ def count_csv_rows(csv_file: str) -> int:
     """
     Return the number of rows in a CSV file.
     """
-    input_file = open(csv_file, "r+")
-    reader_file = csv.reader(input_file)
-    return len(list(reader_file))
+    with open(csv_file, mode="r") as file:
+        return sum(1 for _ in file)
 
 
 def load_csv_data(filename):
@@ -55,6 +54,76 @@ def read_csv_files_in_directory(directory):
             data_list.extend(data)
     numpy_array = np.array(data_list)
     return numpy_array.astype(np.float64)
+
+
+def multifile_loadbalancer(path: str, n: int) -> dict:
+    # os listdir and filter with .csv
+    files = [f for f in os.listdir(path) if f.endswith(".csv")]
+
+    # get total number of rows of each files
+    file_dict = {}
+    for file in files:
+        file_dict[file] = count_csv_rows(os.path.join(path, file))
+
+    # get total number of rows
+    total_rows = sum(file_dict.values())
+
+    # get number of rows per process
+    rows_per_process = total_rows // n
+
+    offset = 0
+    done_files = []
+    final_dict = {}
+
+    for i in range(n):
+        final_dict[i] = []
+
+    # iterate through each process
+    for i in range(n):
+        missing_rows = rows_per_process
+
+        for file in files:
+            if file not in done_files:
+                # check whether the file has enough rows
+                if offset + missing_rows <= file_dict[file]:
+                    # if so, start with the previous offset until the missing rows
+                    final_dict[i].append(
+                        {
+                            "file": file,
+                            "start": offset,
+                            "end": offset + missing_rows,
+                        }
+                    )
+
+                    if offset + missing_rows == file_dict[file]:
+                        # has reached end of the file
+                        done_files.append(file)
+                        offset = 0
+                    else:
+                        offset += missing_rows
+
+                    # break if necessary rows per process has been reached
+                    break
+                else:
+                    final_dict[i].append(
+                        {
+                            "file": file,
+                            "start": offset,
+                            "end": file_dict[file],
+                        }
+                    )
+                    done_files.append(file)
+
+                    missing_rows = file_dict[file] - offset
+                    offset = 0
+                    if missing_rows < 0:
+                        missing_rows = 0
+                        break
+
+    for i in final_dict:
+        print(f"Process {i}: {final_dict[i]}")
+
+    return final_dict
 
 
 if __name__ == "__main__":
